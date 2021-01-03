2d9f79ce5f1da798497040c2c9677a701abf95ad
Viyadalong <l.viyada@gmail.com>
2021-01-03 12:45:47 +0100

    test

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\Actor;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class ActorFixtures extends Fixture implements DependentFixtureInterface {
+    const ACTORS = [
+        'Andrew Lincoln',        
+        'Norman Reedus',
+        'Lauren Cohan',
+        'Danai Gurira', 
+    ];
+
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::ACTORS as $key => $actorName){
+            $actor = new Actor();
+            $actor->setName($actorName);
+            $actor->addProgram($this->getReference('program_1'));
+            $manager->persist($actor);
+        }
+
+        $faker = Faker\Factory::create('fr_FR');
+        
+        for ($i = 0; $i < 50; $i++) {
+            $actor = new Actor();
+            $actor->setName($faker->name);
+            $manager->persist($actor);
+            $actor->addProgram($this->getReference('program_' . $faker->numberBetween(0,5)));
+            $this->addReference('actor_'.$i,$actor);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()  
+    {
+        return [ProgramFixtures::class];  
+    }
+} 
---
+++ src/DataFixtures/SeasonFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+use App\Entity\Season;
+use Faker;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class SeasonFixtures extends Fixture implements DependentFixtureInterface
+{
+
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 50; $i++) {
+            $season = new Season();
+            $season->setNumber($faker->numberBetween(0,10));
+            $season->setDescription($faker->paragraph);
+            $season->setYear($faker->year);
+            $manager->persist($season);
+            $this->setReference('season_'. $i, $season);
+            $season->setProgram($this->getReference('program_'.rand(0,5)));
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [ProgramFixtures::class];
+    }
+} 
